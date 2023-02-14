e07671935b67263f4a52334206027b1f20b7647b
yusuf-ozmen <103484064+yusuf-ozmen@users.noreply.github.com>
2023-02-14 15:54:27 +0100

    test

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ public class Program
 {
     private static void main(string[] args)
     {
-        Console.WriteLine("Welcome to the RPG \n");
+        Console.WriteLine("Welcome to the RPG! \n");
     }
 }
