ef621c65a31e3925284f628f896def11736fa50f
TikTax28 <113066298+TikTax28@users.noreply.github.com>
2023-02-21 13:12:36 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
 public class Program
 {
+    private static Player _player;
+
     private static void main(string[] args)
     {
-        Console.WriteLine("Welcome to the RPG \n");
+         while(true)
+            {
+                Console.Write(">");
+                string userInput = Console.ReadLine();
+
+                if(userInput == null)
+                {
+                    continue;
+                }
+                string cleanedInput = userInput.ToLower();
+
+                if(cleanedInput == "exit")
+                {
+
+                    break;
+                }
+
+                ParseInput(cleanedInput);
+            }
     }
 }
