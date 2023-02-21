247d43c174c506b5cafbca0c313ba07ed6e2e0f2
TikTax28 <113066298+TikTax28@users.noreply.github.com>
2023-02-21 14:07:16 +0100

    Update Monster.cs

--- Monster.cs
+++ Monster.cs
@@ -1,28 +1,29 @@
-public class Monster : LivingCreature
+public class Monster
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string NamePlural { get; set; }
+    public int MaximumDamage { get; set; }
+    public int RewardExperiencePoints { get; set; }
+    public int RewardGold { get; set; }
+
+
+    public List<LootItem> LootTable { get; set; }
+
+    internal List<InventoryItem> LootItems { get; }
+
+    public Monster(int id, string name, string namePlural, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) 
+        : base(currentHitPoints, maximumHitPoints)
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string NamePlural { get; set; }
-        public int MaximumDamage { get; set; }
-        public int RewardExperiencePoints { get; set; }
-        public int RewardGold { get; set; }
+        ID = id;
+        Name = name;
+        NamePlural = namePlural;
+        MaximumDamage = maximumDamage;
+        RewardExperiencePoints = rewardExperiencePoints;
+        RewardGold = rewardGold;
 
-  
-        public List<LootItem> LootTable { get; set; }
+        LootTable = new List<LootItem>();
 
-        internal List<InventoryItem> LootItems { get; }
-
-        public Monster(int id, string name, string namePlural, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) 
-            : base(currentHitPoints, maximumHitPoints)
-        {
-            ID = id;
-            Name = name;
-            NamePlural = namePlural;
-            MaximumDamage = maximumDamage;
-            RewardExperiencePoints = rewardExperiencePoints;
-            RewardGold = rewardGold;
-
-            LootTable = new List<LootItem>();
-
-            LootItems = new List<InventoryItem>();
-        }
+        LootItems = new List<InventoryItem>();
+    }
+}
