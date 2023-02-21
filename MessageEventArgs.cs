119b6e11b692c9e74111a94396d716876aca3bf4
TikTax28 <113066298+TikTax28@users.noreply.github.com>
2023-02-21 13:09:15 +0100

    Add files via upload

---
+++ MessageEventArgs.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MessageEventArgs : EventArgs
+{
+    public string Message { get; private set; }
+    public bool AddExtraNewLine { get; private set; }
+
+    public MessageEventArgs(string message, bool addExtraNewLine)
+    {
+        Message = message;
+        AddExtraNewLine = addExtraNewLine;
+    }
+}
