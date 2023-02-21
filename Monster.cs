41a4009f7eeb5f8b37cbb0b4f80238911725aab3
TikTax28 <113066298+TikTax28@users.noreply.github.com>
2023-02-21 13:29:16 +0100

    Create Monster.cs

---
+++ Monster.cs
@@ -0,0 +1,28 @@
+public class Monster : LivingCreature
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int MaximumDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+
+  
+        public List<LootItem> LootTable { get; set; }
+
+        internal List<InventoryItem> LootItems { get; }
+
+        public Monster(int id, string name, string namePlural, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) 
+            : base(currentHitPoints, maximumHitPoints)
+        {
+            ID = id;
+            Name = name;
+            NamePlural = namePlural;
+            MaximumDamage = maximumDamage;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+
+            LootTable = new List<LootItem>();
+
+            LootItems = new List<InventoryItem>();
+        }
